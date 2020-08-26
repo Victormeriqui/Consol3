12fda954564a04896de1ddd8a6d84cae76b4fdaf
Victor Meriqui <victormeriqui@sapo.pt>
2020-08-27 00:28:23 +0100

    Add Model and Mesh classes

--- Consol3/Model.hpp
+++ Consol3/Model.hpp
@@ -1,14 +1,31 @@
 #ifndef MODEL_HPP
 #define MODEL_HPP
 
+#include "Vertex.hpp"
+
+#include <string>
+#include <vector>
+
 namespace Engine
 {
 	namespace Rendering
 	{
 		class Model
 		{
+		private:
+			std::vector<Vertex> vertices;
+			std::vector<unsigned int> indices;
 
+		public:
+			Model(const std::vector<Vertex>& vertices, const std::vector<unsigned int>& indices);
+			Model(std::string filename);
 
+			void CalculateNormals() const;
+
+			[[nodiscard]] const std::vector<Vertex>& GetVertices() const;
+			[[nodiscard]] const std::vector<unsigned int>& GetIndices() const;
+
+			void DrawModel(/*const Transform& transform*/) const;
 		};
 	}
 }
