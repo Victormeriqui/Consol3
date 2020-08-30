f92a18a2af9484dbd503da03066fb6ebb7e3cf02
Victor Meriqui <victormeriqui@sapo.pt>
2020-08-30 03:37:11 +0100

    Add Greyscale test to main

--- Consol3/Win10ConsoleFrameBufferRenderer.cpp
+++ Consol3/Win10ConsoleFrameBufferRenderer.cpp
@@ -4,7 +4,16 @@
 
 namespace Display
 {
-	Win10ConsoleFrameBufferRenderer::Win10ConsoleFrameBufferRenderer()
+	Win10ConsoleFrameBufferRenderer::Win10ConsoleFrameBufferRenderer(const FrameBuffer& framebuffer) :
+		AbstractFrameBufferRenderer(
+			framebuffer.GetWidth(),
+			framebuffer.GetHeight(),
+			framebuffer.GetWidth(),
+			framebuffer.GetHeight() / 2, // see charbuffer initialization (the inner ConsoleManager needs to know this ahead of time for the CONSOLE_SCREEN_BUFFER_INFOEX struct
+			L"Terminal", // other fonts have bleeding artifacts with lower sizes, this corresponds to "Raster Fonts"
+			4,
+			6 // effectively 4x3 pixel size, TODO: counteract this with the viewport matrix
+		)
 	{
 	}
 
