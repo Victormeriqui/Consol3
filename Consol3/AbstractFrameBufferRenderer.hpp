a95382e1c3f44e904231029b55869d32ec8ac9f0
Victor Meriqui <victormeriqui@sapo.pt>
2020-08-30 03:36:15 +0100

    Change AbstractFrameBuffer to hold the new ConsoleManager

--- Consol3/AbstractFrameBufferRenderer.hpp
+++ Consol3/AbstractFrameBufferRenderer.hpp
@@ -2,23 +2,43 @@
 #define ABSTRACTFRAMEBUFFERRENDERER_HPP
 
 #include "FrameBuffer.hpp"
-#include "ConsoleBuffer.hpp"
+#include "ConsoleManager.hpp"
 
 namespace Display
 {
 	class AbstractFrameBufferRenderer 
 	{
 	protected:
-		const ConsoleBuffer consolebuffer;
+		ConsoleManager consolemanager;
+		uint16_t framebuffer_width;
+		uint16_t framebuffer_height;
 
-		AbstractFrameBufferRenderer() : consolebuffer(ConsoleBuffer()) {}
+		AbstractFrameBufferRenderer(uint16_t framebuffer_width, 
+				uint16_t framebuffer_height,
+				short charbuffer_width, short
+				charbuffer_height,
+				std::wstring font_name,
+				short font_width,
+				short font_height) :
+			consolemanager(
+				ConsoleManager(
+					charbuffer_width,
+					charbuffer_height,
+					font_name,
+					font_width,
+					font_height
+				)
+			),
+			framebuffer_width(framebuffer_width),
+			framebuffer_height(framebuffer_height)
+		{}
 
 		/**
-		* Translates the given frame buffer to the inner consolebuffer held by this class
-		* After translation the consolebuffer is ready to drawn to the console
+		* Translates the given frame buffer to the inner representation needed by the FrameBufferRenderer class
+		* After translation the frame is is ready to drawn
 		*/
-		virtual void TranslateToConsoleBuffer(const FrameBuffer& framebuffer) = 0;
-		virtual void DrawConsoleFrame() = 0;
+		virtual void TranslateFrameForDrawing(const FrameBuffer& framebuffer) = 0;
+		virtual void DrawFrame() = 0;
 	};
 
 }
