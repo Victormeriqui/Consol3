b741d8340af40ef72ad6bdf4e761fc7e16c0cb56
Victor Meriqui <victormeriqui@sapo.pt>
2020-08-31 00:24:19 +0100

    Move constexpr operator overloads to Angle.hpp

--- Consol3/Angle.hpp
+++ Consol3/Angle.hpp
@@ -26,27 +26,122 @@ namespace Engine
 			[[nodiscard]] Angle GetInDegrees() const;
 			[[nodiscard]] Angle GetInRadians() const;
 			
-			constexpr Angle& operator+=(const Angle& other) noexcept;
-			constexpr Angle& operator-=(const Angle& other) noexcept;
-			constexpr Angle& operator/=(const Angle& other) noexcept;
-			constexpr Angle& operator*=(const Angle& other) noexcept;
+			constexpr Angle& operator+=(const Angle& other) noexcept
+			{
+				pitch += other.pitch;
+				yaw += other.yaw;
+				roll += other.roll;
 
-			constexpr Angle& operator+=(float scalar) noexcept;
-			constexpr Angle& operator-=(float scalar) noexcept;
-			constexpr Angle& operator/=(float scalar) noexcept;
-			constexpr Angle& operator*=(float scalar) noexcept;
+				return *this;
+			}
 
-			[[nodiscard]] constexpr Angle operator+(const Angle& other) const noexcept;
-			[[nodiscard]] constexpr Angle operator-(const Angle& other) const noexcept;
-			[[nodiscard]] constexpr Angle operator/(const Angle& other) const noexcept;
-			[[nodiscard]] constexpr Angle operator*(const Angle& other) const noexcept;
+			constexpr Angle& operator-=(const Angle& other) noexcept
+			{
+				pitch -= other.pitch;
+				yaw -= other.yaw;
+				roll -= other.roll;
 
-			[[nodiscard]] constexpr Angle operator+(float scalar) const noexcept;
-			[[nodiscard]] constexpr Angle operator-(float scalar) const noexcept;
-			[[nodiscard]] constexpr Angle operator/(float scalar) const noexcept;
-			[[nodiscard]] constexpr Angle operator*(float scalar) const noexcept;
+				return *this;
+			}
 
-			[[nodiscard]] constexpr bool operator==(const Angle& other) const noexcept;
+			constexpr Angle& operator/=(const Angle& other) noexcept
+			{
+				pitch /= other.pitch;
+				yaw /= other.yaw;
+				roll /= other.roll;
+
+				return *this;
+			}
+
+			constexpr Angle& operator*=(const Angle& other) noexcept
+			{
+				pitch *= other.pitch;
+				yaw *= other.yaw;
+				roll *= other.roll;
+
+				return *this;
+			}
+
+			constexpr Angle& operator+=(float scalar) noexcept
+			{
+				pitch += scalar;
+				yaw += scalar;
+				roll += scalar;
+
+				return *this;
+			}
+
+			constexpr Angle& operator-=(float scalar) noexcept
+			{
+				pitch -= scalar;
+				yaw -= scalar;
+				roll -= scalar;
+
+				return *this;
+			}
+
+			constexpr Angle& operator/=(float scalar) noexcept
+			{
+				pitch /= scalar;
+				yaw /= scalar;
+				roll /= scalar;
+
+				return *this;
+			}
+
+			constexpr Angle& operator*=(float scalar) noexcept
+			{
+				pitch *= scalar;
+				yaw *= scalar;
+				roll *= scalar;
+
+				return *this;
+			}
+
+			[[nodiscard]] constexpr Angle operator+(const Angle& other) const noexcept
+			{
+				return Angle(*this) += other;
+			}
+
+			[[nodiscard]] constexpr Angle operator-(const Angle& other) const noexcept
+			{
+				return Angle(*this) -= other;
+			}
+
+			[[nodiscard]] constexpr Angle operator/(const Angle& other) const noexcept
+			{
+				return Angle(*this) /= other;
+			}
+
+			[[nodiscard]] constexpr Angle operator*(const Angle& other) const noexcept
+			{
+				return Angle(*this) *= other;
+			}
+
+			[[nodiscard]] constexpr Angle operator+(float scalar) const noexcept
+			{
+				return Angle(*this) += scalar;
+			}
+
+			[[nodiscard]] constexpr Angle operator-(float scalar) const noexcept
+			{
+				return Angle(*this) -= scalar;
+			}
+
+			[[nodiscard]] constexpr Angle operator/(float scalar) const noexcept
+			{
+				return Angle(*this) /= scalar;
+			}
+
+			[[nodiscard]] constexpr Angle operator*(float scalar) const noexcept
+			{
+				return Angle(*this) *= scalar;
+			}
+
+			[[nodiscard]] constexpr bool operator==(const Angle& other) const noexcept
+			{
+				return (pitch == other.pitch && yaw == other.yaw && roll == other.roll);
+			}
 		};
 	}
 }
