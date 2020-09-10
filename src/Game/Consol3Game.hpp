a15e9d3ffa1c90dc49497077c72bf4dace240aeb
Victor Meriqui <victormeriqui@sapo.pt>
2020-09-11 00:38:20 +0100

    Added Consol3Engine and Consol3Game

---
+++ src/Game/Consol3Game.hpp
@@ -0,0 +1,38 @@
+#ifndef CONSOL3GAME_HPP
+#define CONSOL3GAME_HPP
+
+#include "../Display/FrameBuffer.hpp"
+#include "../Engine/Rendering/Rasterizer.hpp"
+#include "../Engine/Rendering/Vertex.hpp"
+#include "../Engine/Math/Vector3.hpp"
+#include "../Engine/Rendering/Camera.hpp"
+#include "../Engine/Rendering/Model.hpp"
+#include "../Engine/Rendering/Transform.hpp"
+
+using namespace Display;
+using namespace Engine;
+using namespace Engine::Rendering;
+using namespace Engine::Math;
+
+namespace Game
+{
+	class Consol3Game
+	{
+	private:
+		Rasterizer& rasterizer;
+
+		Camera camera;
+		Model model;
+		Transform transform;
+
+	public:
+
+		Consol3Game(Rasterizer& rasterizer);
+
+		void HandleInput();
+		void Update(long long delta);
+		void Render();
+	};
+}
+
+#endif
