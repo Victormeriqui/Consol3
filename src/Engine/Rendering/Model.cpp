e5d298a5b43c79b15e23fd50225485fd1dea7cac
Victor Meriqui <victormeriqui@sapo.pt>
2020-09-11 00:37:58 +0100

    Add Rasterizer to Model.DrawModel

--- src/Engine/Rendering/Model.cpp
+++ src/Engine/Rendering/Model.cpp
@@ -1,13 +1,16 @@
 #include "Model.hpp"
 
 #include "Transform.hpp"
-
-#include <algorithm>
+#include "Rasterizer.hpp"
 
 namespace Engine
 {
 	namespace Rendering
 	{
+
+		Model::Model()
+		{
+		}
 
 		Model::Model(const std::vector<Vertex>& vertices, const std::vector<unsigned int>& indices)
 		{
@@ -35,9 +38,18 @@ namespace Engine
 			return indices;
 		}
 
-		void Model::DrawModel(const Transform& transform) const
+		void Model::DrawModel(const Transform& transform, Rasterizer& rasterizer) const
 		{
-			//TODO: do this, using the transform
+			rasterizer.SetModelMatrix(transform);
+
+			for (unsigned int i = 0; i < indices.size(); i += 3)
+			{
+				Vertex v0 = vertices[indices[i]];
+				Vertex v1 = vertices[indices[i + 1]];
+				Vertex v2 = vertices[indices[i + 2]];
+
+				rasterizer.RasterizeTriangle(v0, v1, v2, 0xFFFFFF);
+			}
 		}
 
 	}
