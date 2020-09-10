00ea66349f4e612b6e4133b1756b7c2454b44c3e
Victor Meriqui <victormeriqui@sapo.pt>
2020-09-10 21:36:04 +0100

    Fix ToRadians and ToDegrees

--- Consol3/MathUtil.hpp
+++ Consol3/MathUtil.hpp
@@ -12,12 +12,12 @@ namespace Engine
 
 			[[nodiscard]] constexpr inline static float ToRadians(float deg) noexcept
 			{
-				return 180.f * deg / PI;
+				return PI * deg / 180.f;
 			}
 
 			[[nodiscard]] constexpr inline static float ToDegrees(float rad) noexcept
 			{
-				return PI * rad / 180.f;
+				return 180.f * rad / PI;
 			}
 
 			[[nodiscard]] constexpr inline static float Lerp(float val, float min, float max)
