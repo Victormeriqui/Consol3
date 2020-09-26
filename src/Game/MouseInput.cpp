8f49a95da3f284de4f98fc7da74107a0e1bc9b54
Victor Meriqui <victormeriqui@sapo.pt>
2020-09-26 23:54:08 +0100

    Add MouseInput
    Basically a wrapper to the windows API with some utility functions

---
+++ src/Game/MouseInput.cpp
@@ -0,0 +1,51 @@
+#include "MouseInput.hpp"
+
+#include "../Engine/Math/Point2.hpp"
+
+#include <cstdint>
+#include <Windows.h>
+
+namespace Game
+{
+	MouseInput::MouseInput()
+	{};
+
+	Point2 MouseInput::GetMousePosition()
+	{
+		POINT coord = { 0, 0 };
+		GetCursorPos(&coord);
+
+		return Point2((uint16_t)coord.x, (uint16_t)coord.y);
+	}
+
+	void MouseInput::SetMousePosition(const Point2& position)
+	{
+		SetCursorPos(position.x, position.y);
+	}
+
+	Vector2 MouseInput::GetMouseDistanceToCenter()
+	{
+		RECT window = { 0, 0, 0, 0 };
+		GetWindowRect(GetConsoleWindow(), &window);
+
+		Point2 center = Point2(
+			(uint16_t)(window.left + ((window.right - window.left) / 2)),
+			(uint16_t)(window.top + ((window.bottom - window.top) / 2)));
+
+		Point2 mouse_pos = GetMousePosition();
+
+		return Vector2((float)mouse_pos.x - center.x, (float)mouse_pos.y - center.y);
+	}
+
+	void MouseInput::SetMousePositionToCenter()
+	{
+		RECT window = { 0, 0, 0, 0 };
+		GetWindowRect(GetConsoleWindow(), &window);
+
+		Point2 center = Point2(
+			(uint16_t)(window.left + ((window.right - window.left) / 2)),
+			(uint16_t)(window.top + ((window.bottom - window.top) / 2)));
+
+		SetMousePosition(center);
+	}
+}
---
+++ src/Game/MouseInput.hpp
@@ -0,0 +1,27 @@
+#ifndef MOUSEINPUT_HPP
+#define MOUSEINPUT_HPP
+
+#include "../Engine/Math/Point2.hpp"
+
+#include <cstdint>
+#include <Windows.h>
+
+using namespace Engine::Math;
+
+namespace Game
+{
+	class MouseInput
+	{
+	public:
+
+		MouseInput();
+
+		[[nodiscard]] static Point2 GetMousePosition();
+		static inline void SetMousePosition(const Point2& position);
+
+		[[nodiscard]] static Vector2 GetMouseDistanceToCenter();
+		static void SetMousePositionToCenter();
+	};
+}
+
+#endif
