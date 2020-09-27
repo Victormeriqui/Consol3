e9be80fbe6cbd5692adc70613a1bcfae6f821b96
Victor Meriqui <victormeriqui@sapo.pt>
2020-09-27 03:11:17 +0100

    Remove half-block rendering from framebuffer translation
    See 3fac83f48f9af190740f51bfd5b6b00158f8f307

--- src/Display/GreyscaleFrameBufferRenderer.cpp
+++ src/Display/GreyscaleFrameBufferRenderer.cpp
@@ -16,26 +16,21 @@ namespace Display
 			framebuffer.GetWidth(), 
 			framebuffer.GetHeight(),
 			framebuffer.GetWidth(),
-			framebuffer.GetHeight() / 2, // see charbuffer initialization (the inner ConsoleManager needs to know this ahead of time for the CONSOLE_SCREEN_BUFFER_INFOEX struct
-			L"Terminal", // other fonts have bleeding artifacts with lower sizes, this corresponds to "Raster Fonts"
+			framebuffer.GetHeight(),
+			L"Consolas",
 			4,
-			6 // effectively 4x3 pixel size, TODO: counteract this with the viewport matrix
+			4
 		)
 	{
-		// since the char buffer height is divided by 2 we cant have half a cell
-		if ((framebuffer_height & 0x01) == 1)
-			throw "Unsupported odd height for framebuffer renderer";
-
-		// we only need half the vertical resolution in char cells, due to the use of the half blocks
-		charbuffer = std::vector<CHAR_INFO>(framebuffer_width * (framebuffer_height / 2));
+		charbuffer = std::vector<CHAR_INFO>(framebuffer_width * framebuffer_height);
 
 		CHAR_INFO defaultchar;
 		defaultchar.Attributes = 0;
 
-		defaultchar.Char.UnicodeChar = (char)223;// upwards half block
-		defaultchar.Char.AsciiChar = (char)223;
+		defaultchar.Char.UnicodeChar = (char)32;// space
+		defaultchar.Char.AsciiChar = (char)32;
 
-		// set the character of every cell to the half block, the position inside the cell (up or down) will be controled by the background and foreground colors
+		// set every character to space
 		std::fill(charbuffer.begin(), charbuffer.end(), defaultchar);
 
 		consolemanager.SetPalette(palette_default);
@@ -47,9 +42,10 @@ namespace Display
 		float brightness = Color::HexToGreyscale(color);
 
 		// map it to a 0-15 palette index
-		uint8_t index = (uint8_t)((15.0f * brightness) / 255.0f);
+		uint8_t index = 16 + (uint8_t)((15.0f * brightness) / 255.0f);
+		uint8_t background_index = index * 16;
 
-		return index;
+		return background_index;
 	}
 
 	void GreyscaleFrameBufferRenderer::TranslateFrameForDrawing(const FrameBuffer& framebuffer)
@@ -60,26 +56,11 @@ namespace Display
 		{
 			for (uint16_t x = 0; x < framebuffer_width; x++)
 			{
-				// the corresponding Y on the charbuffer for this framebuffer pixel
-				uint16_t charbuffer_y = y / 2;
-
-				DWORD old_attributes = charbuffer[x + charbuffer_y * framebuffer_width].Attributes;
-				uint8_t old_foreground = old_attributes & 0x0F;
-				uint8_t old_background = old_attributes & 0xF0;
-
-				bool odd = y & 0x01;
-
 				// convert the pixel to greyscale palette index
 				WORD color = RGBToGreyscaleIndex(data[x + y * framebuffer_width]);
-
-				// decide whether to affect the background (downwards halfblock) or the foreground (upwards halfblock)
-				if (odd)
-					color = color << 4 | old_foreground;
-				else
-					color = old_background | color;
-
+					
 				// set the final cell attributes
-				charbuffer[x + charbuffer_y * framebuffer_width].Attributes = color;
+				charbuffer[x + y * framebuffer_width].Attributes = color;
 			}
 		}
 	}
